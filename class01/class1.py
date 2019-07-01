9e610a033283d80501d6e7fba771a1b9c9d7bf9a
copyer1998 <2782644531@qq.com>
2019-07-01 12:03:45 +0800

    :ambulance:

---
+++ class01/class1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*-coding:utf-8-*-
+
+"""
+@author: YangZhengqian
+@contact: xx@xx.com
+@software: PyCharm
+@file: class1.py
+@time: 2019/7/1 12:02
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
