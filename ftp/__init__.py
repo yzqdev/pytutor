0146c1a71af3e0c8cf84ba6246e1ce01ec4b0e2a
yzqdev <yzqdev@outlook.com>
2022-02-08 09:34:12 +0800

    add ftp

---
+++ ftp/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*-coding:utf-8-*-
+
+"""
+@Author: YangZhengqian
+@contact: xx@xx.com
+@software: PyCharm
+@file: __init__.py.py
+@time: 2022/2/8 9:13
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
